67bccf49ff5b088aca1319b7b0a3728bda81027b
Ali Abdullah <aliabdullah12347@gmail.com>
2025-07-19 11:06:58 -0400

    Some minor changes, added some features and basic testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 function HomePage() {
   return (
-    <div>
+    <div className="bg-red-100  items-center justify-center text-center tex-blue-300 h-20">
       <h1>Welcome to NotesHelperApp</h1>
       <p>Your personal note-taking assistant.</p>
     </div>
